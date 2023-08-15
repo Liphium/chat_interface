f938c6901328f96fe16446f876e8986e41aaa075
Julian <unfehlerhaft@gmail.com>
2023-08-15 19:04:56 +0200

    New sidebar

--- lib/pages/chat/sidebar/sidebar.dart
+++ lib/pages/chat/sidebar/sidebar.dart
@@ -1,4 +1,4 @@
-import 'package:chat_interface/pages/chat/sidebar/sidebar_button.dart';
+import 'package:chat_interface/pages/chat/sidebar/sidebar_icon_button.dart';
 import 'package:chat_interface/pages/chat/sidebar/sidebar_profile.dart';
 import 'package:chat_interface/pages/chat/sidebar/tabs/conversations/conversations_page.dart';
 import 'package:chat_interface/pages/chat/sidebar/tabs/friends/friends_page.dart';
@@ -16,17 +16,18 @@ class Sidebar extends StatefulWidget {
 
 class _SidebarState extends State<Sidebar> {
 
-  var map = <String, Widget>{
-    'chat.all': const ConversationsPage(),
-    'chat.friends': const FriendsPage(),
-    'chat.everyone': const SettingsPage(),
+  var map = <int, Widget>{
+    0: const ConversationsPage(),
+    1: const FriendsPage(),
+    2: const SettingsPage(),
+    3: const SettingsPage(),
   };
 
   @override
   Widget build(BuildContext context) {
 
     ThemeData theme = Theme.of(context);
-    var selected = 'chat.all'.obs;
+    var selected = 0.obs;
 
     //* Sidebar
     return Container(
@@ -43,38 +44,52 @@ class _SidebarState extends State<Sidebar> {
               child: Padding(
                 padding: const EdgeInsets.all(defaultSpacing),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
           
                     //* Conversations
-                    SidebarButton(
+                    SidebarIconButton(
+                      icon: Icons.chat_bubble,
+                      index: 0,
                       selected: selected,
                       onTap: () {
-                        selected.value = 'chat.all';
+                        selected.value = 0;
                       },
-                      label: 'chat.all',
                       radius: const BorderRadius.only(
                         bottomLeft: Radius.circular(defaultSpacing),
                       )
                     ),
           
                     //* Friends
-                    SidebarButton(
+                    SidebarIconButton(
+                      icon: Icons.people,
+                      index: 1,
                       selected: selected,
                       onTap: () {
-                        selected.value = 'chat.friends';
+                        selected.value = 1;
                       },
-                      label: 'chat.friends',
+                      radius: const BorderRadius.all(Radius.zero)
+                    ),
+
+                    //* Cloud storage
+                    SidebarIconButton(
+                      icon: Icons.folder,
+                      index: 2,
+                      selected: selected,
+                      onTap: () {
+                        selected.value = 2;
+                      },
                       radius: const BorderRadius.all(Radius.zero)
                     ),
 
                     //* Everyone
-                    SidebarButton(
+                    SidebarIconButton(
+                      icon: Icons.public,
+                      index: 3,
                       selected: selected,
                       onTap: () {
-                        selected.value = 'chat.everyone';
+                        selected.value = 3;
                       },
-                      label: 'chat.everyone',
                       radius: const BorderRadius.only(
                         topRight: Radius.circular(defaultSpacing),
                       )
