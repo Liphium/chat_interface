17ca6b1f7e0509ecf39c3b12bf9bed3ebbf2ce82
Julian <unfehlerhaft@gmail.com>
2023-08-19 08:51:21 +0200

    Work is now done. Just testing it left, gonna do that tomorrow

--- lib/connection/impl/stored_actions_util.dart
+++ lib/connection/impl/stored_actions_util.dart
@@ -22,7 +22,23 @@ Future<bool> deleteStoredAction(String id) async {
 
 Future<bool> sendAuthenticatedStoredAction(Friend friend, String payload) async {
 
-  // TODO: Implement  
+  // Send stored action
+  final res = await postRqAuth("/account/stored_actions/send_auth", <String, dynamic>{
+    "account": friend.id,
+    "payload": payload,
+    "key": friend.keyStorage.storedActionKey,
+  }, randomRemoteID());
+
+  if(res.statusCode != 200) {
+    sendLog("couldn't send stored action: invalid request");
+    return false;
+  }
+
+  final json = jsonDecode(res.body);
+  if(!json["success"]) {
+    sendLog("couldn't send stored action: ${json["error"]}");
+    return false;
+  }
 
   return true;
 }
