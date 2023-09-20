45dac1e7880dabd3bf414a47d71dc24de39612a3
Julian <unfehlerhaft@gmail.com>
2023-09-20 17:24:41 +0200

    Remove voice_windows

--- voice_windows/lib/voice_windows.dart
+++
@@ -1,8 +0,0 @@
-
-import 'voice_windows_platform_interface.dart';
-
-class VoiceWindows {
-  Future<String?> getPlatformVersion() {
-    return VoiceWindowsPlatform.instance.getPlatformVersion();
-  }
-}
