25534ee5a6e0b5f8040f38a1a74f6bf69c16894d
Julian <unfehlerhaft@gmail.com>
2023-10-08 16:35:05 +0200

    Setup translations

--- interface/lib/translations/setup.dart
+++ interface/lib/translations/setup.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 class SetupTranslations extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
+
+    //* English US
     'en_US': {
       // Error page
       'retry.text.1': 'Trying again in',
@@ -11,6 +13,17 @@ class SetupTranslations extends Translations {
       // Instance setup
       'setup.choose.instance': 'Choose an instance.',
       'setup.instance.name': 'Enter a new name'
+    },
+
+    //* German
+    'de_DE': {
+      // Error page
+      'retry.text.1': 'Versuche es in',
+      'retry.text.2': 'Sekunden erneut.',
+
+      // Instance setup
+      'setup.choose.instance': 'Wähle eine Instanz.',
+      'setup.instance.name': 'Gib einen Namen ein'
     }
   };
 }
