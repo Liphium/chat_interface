8285386adf7f9b036defbc120b123a43c9aac36e
Julian <unfehlerhaft@gmail.com>
2024-04-01 14:20:13 +0200

    Powershell now overwrites the archive no matter what

--- scripts/windows_package_zip.ps1
+++ scripts/windows_package_zip.ps1
@@ -1,3 +1,3 @@
 flutter build windows --release
 cd ..\build\windows\x64\runner\Release
-Compress-Archive -Path .\* -DestinationPath "$HOME\Downloads\windows.zip"
+Compress-Archive -Path .\* -DestinationPath "$HOME\Downloads\windows.zip" -Force
