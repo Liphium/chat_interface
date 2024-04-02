34962190707d12115f268c89a8e40fed245aa51a
Unbreathable <unfehlerhaftgmail.com>
2024-04-02 14:58:14 +0200

    macOS good value

--- rust/src/audio/microphone.rs
+++ rust/src/audio/microphone.rs
@@ -112,17 +112,13 @@ pub fn record() {
                     }
                 }
 
+                // macOS default value: 0.79
+
                 // Detect if the user is talking
                 let talking: bool = if options.detection_mode == 0 {
                     let probability = vad.predict(samples);
 
-                    if probability > 0.1 {
-                        prob_streak += 1;
-                    } else {
-                        prob_streak = 0;
-                    }
-
-                    prob_streak > 5
+                    probability > 0.79
                 } else {
                     max > options.talking_amplitude
                 };
